d6f2a1449e91b811c2d2755caf3e8fe5f36e9e67
Kyle <kyleomondi@gmail.com>
2026-09-26 01:57:20 +0300

    update

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ login_manager.init_app(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro',
                      force_default=False, force_lower=False,
                      use_ssl=False, base_url=None)
-
+with app.app_context():
+    db.create_all()
+    print("Tables created.")
 
 @login_manager.user_loader
 def load_user(user_id):
